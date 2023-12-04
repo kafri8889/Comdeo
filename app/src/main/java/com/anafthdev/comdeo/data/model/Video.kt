54ca047c227490b58e290b2713f0f220029fb1f6
kafri8889 <kafri8889@gmail.com>
2023-12-04 15:47:47 +0700

    Add video info

--- app/src/main/java/com/anafthdev/comdeo/data/model/Video.kt
+++ app/src/main/java/com/anafthdev/comdeo/data/model/Video.kt
@@ -25,4 +25,9 @@ data class Video(
     @ColumnInfo(name = "dateAdded_video") val dateAdded: Long,
     @ColumnInfo(name = "duration_video") val duration: Long,
     @ColumnInfo(name = "size_video") val size: Long,
+    @ColumnInfo(name = "width_video") val width: Int,
+    @ColumnInfo(name = "height_video") val height: Int,
+    @ColumnInfo(name = "resolution_video") val resolution: String?,
+    @ColumnInfo(name = "bitrate_video") val bitrate: Int,
+    @ColumnInfo(name = "relativePath_video") val relativePath: String,
 ): Parcelable
--- app/src/main/java/com/anafthdev/comdeo/util/VideoUtil.kt
+++ app/src/main/java/com/anafthdev/comdeo/util/VideoUtil.kt
@@ -3,6 +3,7 @@ package com.anafthdev.comdeo.util
 import android.content.Context
 import android.net.Uri
 import android.provider.MediaStore
+import androidx.core.database.getStringOrNull
 import com.anafthdev.comdeo.data.model.Video
 
 object VideoUtil {
@@ -17,13 +18,12 @@ object VideoUtil {
                 MediaStore.Video.Media.DATE_MODIFIED,
                 MediaStore.Video.Media.DURATION,
                 MediaStore.Video.Media.SIZE,
+                MediaStore.Video.Media.WIDTH,
+                MediaStore.Video.Media.HEIGHT,
+                MediaStore.Video.Media.RESOLUTION,
+                MediaStore.Video.Media.BITRATE,
+                MediaStore.Video.Media.RELATIVE_PATH,
             )
-
-            val cursorIndexVideoID: Int
-            val cursorIndexVideoDisplayName: Int
-            val cursorIndexVideoDateAdded: Int
-            val cursorIndexVideoDuration: Int
-            val cursorIndexVideoSize: Int
 
             val cursor = context.contentResolver.query(
                 externalContentUri,
@@ -34,11 +34,16 @@ object VideoUtil {
             )
 
             if (cursor != null) {
-                cursorIndexVideoID = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
-                cursorIndexVideoDisplayName = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
-                cursorIndexVideoDateAdded = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_MODIFIED)
-                cursorIndexVideoDuration = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION)
-                cursorIndexVideoSize = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE)
+                val cursorIndexVideoID = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+                val cursorIndexVideoDisplayName = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
+                val cursorIndexVideoDateAdded = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_MODIFIED)
+                val cursorIndexVideoDuration = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION)
+                val cursorIndexVideoSize = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE)
+                val cursorIndexVideoWidth = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.WIDTH)
+                val cursorIndexVideoHeight = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.HEIGHT)
+                val cursorIndexVideoResolution = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.RESOLUTION)
+                val cursorIndexVideoBitrate = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BITRATE)
+                val cursorIndexVideoRelativePath = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.RELATIVE_PATH)
 
                 while (cursor.moveToNext()) {
                     val id = cursor.getLong(cursorIndexVideoID)
@@ -46,6 +51,11 @@ object VideoUtil {
                     val dateAdded = cursor.getLong(cursorIndexVideoDateAdded)
                     val duration = cursor.getLong(cursorIndexVideoDuration)
                     val size = cursor.getLong(cursorIndexVideoSize)
+                    val width = cursor.getInt(cursorIndexVideoWidth)
+                    val height = cursor.getInt(cursorIndexVideoHeight)
+                    val resolution = cursor.getStringOrNull(cursorIndexVideoResolution)
+                    val bitrate = cursor.getInt(cursorIndexVideoBitrate)
+                    val relativePath = cursor.getString(cursorIndexVideoRelativePath)
 
                     val path = Uri.withAppendedPath(externalContentUri, id.toString()).toString()
 
@@ -56,7 +66,12 @@ object VideoUtil {
                             displayName = displayName,
                             dateAdded = dateAdded * 1000,
                             duration = duration,
-                            size = size
+                            size = size,
+                            width = width,
+                            height = height,
+                            resolution = resolution,
+                            bitrate = bitrate,
+                            relativePath = relativePath,
                         )
                     )
                 }
